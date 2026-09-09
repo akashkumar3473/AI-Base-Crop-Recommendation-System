e265dd06a549e5261cf7273271bb70c8d5116457
akashkumar3473@ <akashsingh34685@gmail.com>
2026-09-10 01:31:24 +0530

    Fix production API URL

--- frontend/src/components/CropRecommendation.jsx
+++ frontend/src/components/CropRecommendation.jsx
@@ -81,7 +81,7 @@ const CropRecommendation = () => {
     try {
       setLoading(true);
       const response = await fetch(
-        "http://127.0.0.1:5000/predict",
+  `${import.meta.env.VITE_API_URL}/predict`,
         {
           method: "POST",
           headers: {
